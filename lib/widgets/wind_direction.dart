ef27b3048a36f0d1e5570f24e116c725608a9e29
Artemiy Martyanov <artemiy@martyanov.xyz>
2022-04-10 13:22:19 +0300

    add bottom sheet for wind direction

--- lib/widgets/wind_direction.dart
+++ lib/widgets/wind_direction.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forecast_app/blocs/common_bloc/common_bloc.dart';
 import 'package:forecast_app/cubits/cubit/wind_cubit.dart';
 import 'package:forecast_app/widgets/compass_painter.dart';
+import 'package:forecast_app/widgets/wind_bottom_sheet.dart';
 
 class WindDirection extends StatelessWidget {
   const WindDirection({Key? key}) : super(key: key);
@@ -13,33 +14,38 @@ class WindDirection extends StatelessWidget {
       builder: (commonBlocContext, commonBlocState) {
         return BlocBuilder<WindCubit, WindState>(
           builder: (windCubitContext, windCubitState) {
-            return Card(
-              elevation: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Направл. ветра",
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    const SizedBox(
-                      height: 5,
-                      width: 5,
-                    ),
-                    commonBlocState.isFetching
-                        ? const CircularProgressIndicator()
-                        : SizedBox(
-                            width: 60,
-                            height: 60,
-                            child: CustomPaint(
-                              painter: CompassPainter(
-                                  rotate: windCubitState.direction),
+            return GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (_) {
+                    return const WindBottomSheet(
+                      description: 'Направление ветра',
+                    );
+                  },
+                );
+              },
+              child: Card(
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      commonBlocState.isFetching
+                          ? const CircularProgressIndicator()
+                          : SizedBox(
+                              width: 80,
+                              height: 80,
+                              child: CustomPaint(
+                                painter: CompassPainter(
+                                    rotate: windCubitState.direction),
+                              ),
                             ),
-                          ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
